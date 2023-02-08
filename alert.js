9d6f866a8d8a7391deb909109d559676e60dacb9
alina126user <yablochnikova18@gmail.com>
2023-02-08 21:29:05 +0200

    edit variables

--- alert.js
+++ alert.js
@@ -3,9 +3,13 @@ let user = 'John',
 age = 25, 
 message = 'Hello!';
 
-let admin;
-let name = 'John';
-admin = 'John'
+let admin, name8;
+name8 = 'John';
+admin = name8;
+
+let thirdPlanet = 'Earth';
+let visitorName = 'Harry';
+
 
 alert(admin);
 
